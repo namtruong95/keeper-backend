cab205d48c20988da604db9f112344182487b092
Truong Thanh Nam <nam.vboys@gmail.com>
2017-08-27 13:20:51 +0700

    config connect database

--- config/database.js
+++ config/database.js
@@ -68,7 +68,7 @@ module.exports = {
   */
   pg: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://jigsmugciiuyqe:e8692bb2ba59fd4bac4f06a853cb0da74678c66bda8a9d5acd5c457349584662@ec2-54-163-254-143.compute-1.amazonaws.com:5432/da9kaad5fts9qk'
+    connection: 'postgres://apnzfxuhjirnnm:47a4c2a1ff1fb50b6f2481878ea03c9885d1d6928cc2061e1671015a957e9435@ec2-54-163-254-143.compute-1.amazonaws.com:5432/dbtchqplvelfjr'
   }
 
 }
