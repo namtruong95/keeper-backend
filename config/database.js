54a24b858b4f0cd935ca9b85636f5c42406c5724
Truong Thanh Nam <nam.vboys@gmail.com>
2017-08-27 20:56:42 +0700

    set env variables

--- config/database.js
+++ config/database.js
@@ -14,7 +14,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get('DB_CONNECTION', 'pg'),
 
   /*
   |--------------------------------------------------------------------------
@@ -47,13 +47,13 @@ module.exports = {
   */
   mysql: {
     client: 'mysql',
-    connection: {
+    connection: Env.get('DATABASE_URL', {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
       database: Env.get('DB_DATABASE', 'adonis')
-    }
+    })
   },
 
   /*
