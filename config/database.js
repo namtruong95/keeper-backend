45d2752cdcb47bcdf344bf5893fd67a1eaf3da27
Truong Thanh Nam <nam.vboys@gmail.com>
2017-08-27 13:26:15 +0700

    set default database is pg

--- config/database.js
+++ config/database.js
@@ -14,7 +14,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'pg'),
+  connection: 'pg',
 
   /*
   |--------------------------------------------------------------------------
@@ -68,7 +68,7 @@ module.exports = {
   */
   pg: {
     client: 'pg',
-    connection: 'postgres://apnzfxuhjirnnm:47a4c2a1ff1fb50b6f2481878ea03c9885d1d6928cc2061e1671015a957e9435@ec2-54-163-254-143.compute-1.amazonaws.com:5432/dbtchqplvelfjr'
+    connection: process.env.DATABASE_URL || 'postgres://apnzfxuhjirnnm:47a4c2a1ff1fb50b6f2481878ea03c9885d1d6928cc2061e1671015a957e9435@ec2-54-163-254-143.compute-1.amazonaws.com:5432/dbtchqplvelfjr'
   }
 
 }
